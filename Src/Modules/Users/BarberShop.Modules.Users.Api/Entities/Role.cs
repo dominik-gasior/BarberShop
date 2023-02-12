8cb2430520bd70a4e1f384456f785cfb90dbb1c3
Dominik <dominikgasior03@gmail.com>
2023-02-12 08:50:24 +0100

    change enum and bash

--- Src/Modules/Users/BarberShop.Modules.Users.Api/Entities/Role.cs
+++ Src/Modules/Users/BarberShop.Modules.Users.Api/Entities/Role.cs
@@ -3,8 +3,8 @@ namespace BarberShop.Modules.Users.Api.Entities
     internal enum Role
     {
         Klient = 1,
-        Pracownik = 2,
-        Menadzer = 3,
-        Właściciel = 4
+        Pracownik,
+        Menadzer,
+        Wlasciciel 
     }
 }
