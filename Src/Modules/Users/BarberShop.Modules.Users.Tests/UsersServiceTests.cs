71ef20fc8521e85cee4ba7b9cc7e959657f90a7f
Dominik <dominikgasior03@gmail.com>
2023-02-22 18:51:57 +0100

    start integration test (version test)

---
+++ Src/Modules/Users/BarberShop.Modules.Users.Tests/UsersServiceTests.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BarberShop.Modules.Users.Tests;
+
+public class UsersServiceTests
+{
+    private readonly HttpClient _client;
+
+    public UsersServiceTests()
+    {
+        var factory = new WebApplicationFactory<Program>();
+        _client = factory.CreateDefaultClient();
+    }
+
+    [Fact]
+    public async Task GetAllUsers_FetchUsers_ReturnStatusOk()
+    {
+        //act
+        var response = await _client.GetAsync("/api/getAllUsers");
+
+        //assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        response.Content.Should().NotBeNull();
+    }
+    // [Theory]
+    // [InlineData("123")]
+    // public async Task GetUserById_FetchUser_ReturnStatusOk(string id)
+    // {
+    //     //act
+    //     var response = await _client.GetAsync("/api/user/" + id);
+    //
+    //     //assert
+    //     response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    //     // response.Content.Should().NotBeNull();
+    // }
+}
